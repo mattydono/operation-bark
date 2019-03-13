6b2c67a89f48983ab714e55bb9b68a2618aafd96
Matthew <matthew.donovan@mac.com>
2019-03-13 12:36:06 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+          Hi
+      </div>
+    );
+  }
+}
+
+export default App;
