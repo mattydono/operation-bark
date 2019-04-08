6a8ff073b156d26a8586516775b50094c41edfd2
Matthew <matthew.donovan@mac.com>
2019-04-08 16:41:18 +0100

    feat: password fields + validation added

--- src/Registration/Registration.tsx
+++ src/Registration/Registration.tsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React, { ChangeEventHandler, Component } from 'react';
+=======
+import React, { useCallback, useState } from 'react';
+>>>>>>> feat: password fields + validation added
 import { connect } from 'react-redux';
 import { User, UserType } from '../models';
 import { AppState } from '../redux/configureStore';
@@ -14,6 +18,7 @@ type DispatchProps = {
 
 type Props = StateProps & DispatchProps;
 
+<<<<<<< HEAD
 type State = {
   email: string;
   firstName: string;
@@ -78,12 +83,80 @@ export class _Registration extends Component<Props, State> {
 
   private onAddUser = () => {
     const user = { ...this.state };
+=======
+const _Registration = (props: Props) => {
+  const [firstName, onFirstNameChange] = useField('');
+  const [surname, onSurnameChange] = useField('');
+  const [type, onTypeChange] = useField<UserType | null, HTMLSelectElement>(null);
+  const [email, onEmailChange] = useField('');
+  const [password, onPasswordChange] = useField('');
+  const [confirmPassword, onConfirmPasswordChange] = useField('');
+  const [isValid, setIsValid] = useState(false);
+
+  const onAddUser = useCallback(() => {
+    const user = { email, firstName, surname, type, password, confirmPassword };
+>>>>>>> feat: password fields + validation added
     if (user.type !== null) {
       // Typescript not registering null check -> as User
       return this.props.addUser(user as User);
     }
+<<<<<<< HEAD
   };
 }
+=======
+  }, [email, firstName, surname, type, password, confirmPassword]);
+
+  const disabled =
+    type === null ||
+    firstName === '' ||
+    surname === '' ||
+    email === '' ||
+    password === '' ||
+    confirmPassword === '' ||
+    isValid === false;
+
+  // const [value, onValueChange] = useField('');
+  // const [isValid, setIsValid] = useState(false);
+
+  // const onChange = useCallback(event => {
+  //    isValidCcyPair(event.currentTarget.value).then(setIsValid);
+  //    onValueChange(event;
+  //    setIsValid(false);
+  // }, []);
+
+  const onChange = useCallback(
+    event => {
+      setIsValid(event.currentTarget.value === password);
+      onConfirmPasswordChange(event);
+    },
+    [password, setIsValid, onConfirmPasswordChange],
+  );
+
+  return (
+    <div>
+      <input value={firstName} placeholder='firstname' onChange={onFirstNameChange} />
+      <input value={surname} placeholder='surname' onChange={onSurnameChange} />
+      <input value={email} placeholder='email' onChange={onEmailChange} />
+      <input type='password' value={password} placeholder='password' onChange={onPasswordChange} />
+      <input
+        type='password'
+        value={confirmPassword}
+        placeholder='confirmPassword'
+        onChange={onChange}
+      />
+      <select value={type || ''} onChange={onTypeChange}>
+        <option value=''>Select...</option>
+        {Object.values(UserType).map(userType => (
+          <option value={userType}>{userType}</option>
+        ))}
+      </select>
+      <button disabled={disabled} onClick={onAddUser}>
+        Submit
+      </button>
+    </div>
+  );
+};
+>>>>>>> feat: password fields + validation added
 
 const mapStateToProps = (state: AppState): StateProps => ({
   users: state.users.users,
